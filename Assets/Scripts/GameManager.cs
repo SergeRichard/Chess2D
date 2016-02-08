95744daa8611ae43982c27ffe6f852e2c6992c17
serge richard <serge.richard@rogers.com>
2016-02-07 20:27:03 -0400

    Instantiation of board (in progress).
    Instantiation of board (in progress).

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,38 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+
 
 public class GameManager : MonoBehaviour {
 
 	enum State {WhiteSelectionState, WhiteDestinationState, BlackSelectionState, BlackDestinationState};
 	State state = State.WhiteSelectionState;
 
+	public static string[,] board = new string[,] { 
+		{"BR","BN","BB","BK","BQ","BB","BN","BR"},
+		{"BP","BP","BP","BP","BP","BP","BP","BP"},
+		{"--","--","--","--","--","--","--","--"},
+		{"--","--","--","--","--","--","--","--"},
+		{"--","--","--","--","--","--","--","--"},
+		{"--","--","--","--","--","--","--","--"},
+		{"WP","WP","WP","WP","WP","WP","WP","WP"},
+		{"WR","WN","WB","WK","WQ","WB","WN","WR"},
+	};
+	[System.Serializable]
+	public class Pieces {
+
+		public string name;
+		public string column;
+		public string row;
+		public float xpos;
+		public float ypos;
+	}
+
 	// Use this for initialization
 	void Start () {
-		// TODO Initialize board.
+		//foreach (var piece in board) {
+
+
+		//}
 
 	}
 
